e1bd2efd2f3e5b8bb75a6b08f445e3fe370e04bb
Vincent Herail <vincent.herail@gmail.com>
2021-01-06 14:50:57 +0100

    section 4 finished

--- section_4_reaction_timer/src/App.vue
+++ section_4_reaction_timer/src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <!-- can not start a new game, while current game is on -->
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results :score="score" v-if="showResults"/>
 </template>
 
 <script>
+import Block from './components/Block'
+import Results from './components/Results'
 
 export default {
   name: 'App',
-  components: {
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score : null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      // a chaque fois que l'on joue, cela créera un delay aléatoire entre 2 et 7 s
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    // l'event 'end' emit depuis Block est écouté dans la balise Block, il déclenche la fonction et récupère la durée de raction
+    endGame(reactionTime){
+      this.score = reactionTime
+      console.log(this.score)
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,4 +48,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
--- section_4_reaction_timer/src/components/Results.vue
+++ section_4_reaction_timer/src/components/Results.vue
@@ -1,3 +1,32 @@
 <template>
-    
+    <p>Reaction Time - {{ score }} ms</p>
+    <div class="rank">{{ rank }}</div>
 </template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank : null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 450) {
+            this.rank = 'Ninja Kido'
+        } else {
+            this.rank = 'Lazy laze'
+        }
+    }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
