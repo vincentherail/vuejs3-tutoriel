d7d64850dbc6c2300b9a72e99102fe50d198a49c
Vincent Herail <vincent.herail@gmail.com>
2021-01-06 08:45:42 +0100

    section 3 finished

--- section_3_Vue_CLI/modal-project/src/App.vue
+++ section_3_Vue_CLI/modal-project/src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+        <h1>Ninja Ninja</h1>
+        <p>Grab you rNinja swag for half the price !</p>
+        <template v-slot:links>
+          <a href="#">Sign up</a>
+          <a href="#">More infos</a>
+        </template>
+    </Modal>
+  </teleport>
+  <div v-if="showModalTwo">
+    <Modal theme="sale" @close="toggleModalTwo">
+        <template v-slot:links>
+          <a href="#">You did it</a>
+        </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal</button><br/>
+  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My first Vue App:)',
+      header: 'modal title',
+      text: 'hello I am the prop beign passed',
+      showModal : false,
+      showModalTwo : false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.boo.classList.add('active')
+      console.log(this.$refs.boo)
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
