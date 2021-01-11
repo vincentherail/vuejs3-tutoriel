87431299ffe9dd5dc22e517f37b9f86fd13464ce
Vincent Herail <vincent.herail@gmail.com>
2021-01-11 11:11:31 +0100

    section 7 fetching data finished

--- section_6_router/src/views/jobs/Jobs.vue
+++ section_6_router/src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" @key="id" class="job">
-      <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-          <h2>{{job.title}}</h2>
-      </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+            <h2>{{job.title}}</h2>
+        </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs ....</p>
   </div>
 </template>
 
@@ -11,12 +16,15 @@
 export default {
     data() {
         return {
-            jobs: [
-                {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-                {title: 'Ninja Web Designer', id: 2, details: 'lorem'},
-                {title: 'Ninja Vue Dev', id: 3, details: 'lorem'}
-            ]
+          // array vide que l'on rempli avec les datas, puis parcourt pour les afficher dans la vue
+            jobs: []
         }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs')
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
     }
 }
 </script>
