a594e05263dd62106c20426f1fa08e8009f14acd
Vincent Herail <vincent.herail@gmail.com>
2021-01-15 12:24:29 +0100

    section 9 created

---
+++ section_9_dojo_blogs/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    HOME
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  // setup will run before mounted or created
+  setup() {
+
+  }
+}
+</script>
