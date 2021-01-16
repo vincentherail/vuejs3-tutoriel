9312d173ed2651f8f70b1555ce68448eaf0e4d37
Vincent Herail <vincent.herail@gmail.com>
2021-01-16 21:05:30 +0100

    section 9 almost done

--- section_9_dojo_blogs/src/views/Home.vue
+++ section_9_dojo_blogs/src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
-    HOME
+    <h1>Home</h1>
+      <input type="text" v-model="search">
+      <p>search term - {{ search }}</p>
+      <div v-for="name in matchingNames" :key="name">{{name}}</div>
+      <button @click="handleClick">Stop watch</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { computed, ref, watch, watchEffect } from 'vue'
 export default {
   name: 'Home',
-  // setup will run before mounted or created
-  setup() {
+  setup() {    
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'jumbo', 'babar', 'sandy'])
 
-  }
+    const stopWatch = watch(search, () => {
+      console.log('watch function run')
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watch effect ran', search.value)
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return { names, search, matchingNames, handleClick }
+  },
 }
 </script>
