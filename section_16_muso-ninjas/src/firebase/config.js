98a2be2d04af146baefa3c476881b8af15b4986e
Vincent Herail <vincent.herail@gmail.com>
2021-01-28 14:20:54 +0100

    section 16 created

---
+++ section_16_muso-ninjas/src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+// storage sera intégré plus tard
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAN3ZNh8Vb-fl3K0dgUThz9mjsOYCfzx7Q",
+  authDomain: "muso-ninjas-b6131.firebaseapp.com",
+  projectId: "muso-ninjas-b6131",
+  storageBucket: "muso-ninjas-b6131.appspot.com",
+  messagingSenderId: "60941302678",
+  appId: "1:60941302678:web:2352bc24cfe3f89d8d3bb7"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp}
