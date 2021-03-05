d6adae2278504246eb9a828d4d47d550a630e873
Vincent Herail <vincent.herail@gmail.com>
2021-03-05 23:09:47 +0100

    librairie GSAP

--- section_animation_transition/src/views/About.vue
+++ section_animation_transition/src/views/About.vue
@@ -2,54 +2,45 @@
   <div class="about">
     <transition 
       appear 
-      name="fade"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
-            >
-      <h1 v-if="showTitle">About</h1>
+      @afterEnter="afterEnter"
+    >
+      <h1 >About</h1>
     </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import gsap from 'gsap'
 
 export default {
   setup() {
-    const showTitle = ref(true)
 
-    // el représente l'elément DOM activé par le hook, ici h1
     const beforeEnter = (el) => {
-      console.log('beforeEnter', el)
+      console.log('before enter - set initial state')
+      el.style.transform ='translateY(-60px)'
+      el.style.opacity = 0
     }
-    const enter = (el) => {
-      console.log('enter', el)
+    const enter = (el, done) => {
+      console.log('starting enter - make transition')
+      gsap.to(el, {
+        duration: 3,
+        y: 0,
+        opacity: 1,
+        // fait rebondir l'element
+        ease: 'bounce.out',
+        onComplete: done
+      })
     }
     const afterEnter = (el) => {
-      el.style.color = 'green'
-      console.log('after enter', el)
+      console.log('after enter ')
 
-      setTimeout(() => showTitle.value = false, 2000)
-    }
-    const beforeLeave = (el) => {
-      console.log('beforeleave', el)      
-    }
-    const leave = (el) => {
-      el.style.color = 'pink'
-      console.log('leave', el)      
-    }
-    const afterLeave = (el) => {
-      console.log('after leave', el)
     }
 
-    return { showTitle, beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave}
+    return {  beforeEnter, enter, afterEnter }
   }
 }
 </script>
@@ -60,16 +51,5 @@ export default {
     margin: 20px auto;
   }
 
-  .fade-enter-from{
-    opacity: 0;
-  }
-  .fade-enter-active{
-    transition : opacity 3s ease;
-  }
-  .fade-leave-to{
-    opacity: 0;
-  }
-  .fade-leave-active{
-    transition : opacity 3s ease;
-  }
+
 </style>
