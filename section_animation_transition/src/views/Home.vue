2f1ec97d5bed17908a26551ccfef022a71e30d7f
Vincent Herail <vincent.herail@gmail.com>
2021-03-05 19:44:22 +0100

    Adding CSS Keyframes : tremblement gauche-droite

--- section_animation_transition/src/views/Home.vue
+++ section_animation_transition/src/views/Home.vue
@@ -4,7 +4,7 @@
       <Toast v-if="showToast" />
     </transition>
     <Todos @badValue="triggerToast" />
-    <div></div>
+
   </div>
 </template>
 
@@ -29,5 +29,45 @@ export default {
 </script>
 
 <style>
+/* enter classes */
 
+/* PLUS NECESSAIRE DESORMAIS */
+
+/* .toast-enter-from{
+  opacity: 0;
+  bring up from the screen
+  transform: translateY(-60px)
+}
+.toast-enter-to{
+  opacity: 1;
+  bring it back down in the screen
+  transform: translateY(0)
+} */
+.toast-enter-active{
+  /* transition: all 0.3s ease; */
+  animation : wobble 0.5s ease;
+}
+
+/* leave classes */
+.toast-leave-from{
+  opacity: 1;
+  transform: translateY(0)
+}
+.toast-leave-to{
+  opacity: 0;
+  transform: translateY(-60px)
+}
+.toast-leave-active{
+  transition: all 0.3s ease;
+}
+
+@keyframes wobble {
+  0% { transform: translateY(-60px); opacity: 0}
+  50% { transform: translateY(0px); opacity: 1}
+  60% { transform: translateX(8px) }
+  70% { transform: translateX(-8px) }
+  80% { transform: translateX(4px) }
+  90% { transform: translateX(-4px) }
+  100% { transform: translateX(0) }
+}
 </style>
